12b3329839bac3cb000b2890713daa739657eb79
Kakajan <kakajantayjikov@gmail.com>
2024-11-04 08:12:57 +0500

    animation

--- js/script.js
+++ js/script.js
@@ -1,33 +1,43 @@
-const arrows = document.querySelectorAll(".main-arrow");
+// document.addEventListener("DOMContentLoaded", function () {
 
-const texts = document.querySelectorAll(".main-text");
-let currentIndex = 0;
+  // const texts = document.querySelectorAll(".main-text");
 
-function showNextText() {
-  if (currentIndex < texts.length) {
-    texts[currentIndex].style.opacity = 1;
-    currentIndex++;
-    setTimeout(showNextText, 1000);
-  }
-}
+  // function showNextText() {
+  //   if (currentIndex < texts.length) {
+  //     texts[currentIndex].style.opacity = 1;
+  //     currentIndex++;
+  //     setTimeout(showNextText, 1000);
+  //   }
+  // }
 
-showNextText();
+  // showNextText();
 
-const video = document.getElementById("main-video");
+  const texts = document.querySelectorAll(".main-text");
+  let delay = 500; // задержка между появлениями в миллисекундах
 
-video.src =
-  "./source/Gen_3_Alpha_Turbo_1125522594,_Make_an_action_anima,_DALL·E_2024.mp4";
-video.addEventListener("ended", function () {
-  video.currentTime = 0;
-  video.play();
-});
+  texts.forEach((item, index) => {
+    setTimeout(() => {
+      item.style.opacity = 1; // установить видимость
+      item.style.animationDelay = `${index * delay}ms`; // установить задержку
+      item.classList.add("appear"); // добавить класс для анимации
+    }, index * delay);
+  });
 
+  const video = document.getElementById("main-video");
 
-// const video2 = document.getElementById("second");
+  video.src =
+    "./source/Gen_3_Alpha_Turbo_1125522594,_Make_an_action_anima,_DALL·E_2024.mp4";
+  video.addEventListener("ended", function () {
+    video.currentTime = 0;
+    video.play();
+  });
 
-// video2.src =
-//   "./source/Gen_3_Alpha_Turbo_669305172,_Make_an_action_anima,_DALL·E_2024_10.mp4";
-// video2.addEventListener("ended", function () {
-//   video2.currentTime = 0;
-//   video2.play();
+  // const video2 = document.getElementById("second");
+
+  // video2.src =
+  //   "./source/Gen_3_Alpha_Turbo_669305172,_Make_an_action_anima,_DALL·E_2024_10.mp4";
+  // video2.addEventListener("ended", function () {
+  //   video2.currentTime = 0;
+  //   video2.play();
+  // });
 // });
