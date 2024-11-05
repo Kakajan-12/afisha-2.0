76dca635d457698f2ae0ecea3ec8505279cbcf4a
Kakajan <kakajantayjikov@gmail.com>
2024-11-06 00:36:06 +0500

    done project

--- js/script.js
+++ js/script.js
@@ -1,25 +1,11 @@
-// document.addEventListener("DOMContentLoaded", function () {
-
-  // const texts = document.querySelectorAll(".main-text");
-
-  // function showNextText() {
-  //   if (currentIndex < texts.length) {
-  //     texts[currentIndex].style.opacity = 1;
-  //     currentIndex++;
-  //     setTimeout(showNextText, 1000);
-  //   }
-  // }
-
-  // showNextText();
-
   const texts = document.querySelectorAll(".main-text");
-  let delay = 500; // задержка между появлениями в миллисекундах
+  let delay = 500;
 
   texts.forEach((item, index) => {
     setTimeout(() => {
-      item.style.opacity = 1; // установить видимость
-      item.style.animationDelay = `${index * delay}ms`; // установить задержку
-      item.classList.add("appear"); // добавить класс для анимации
+      item.style.opacity = 1;
+      item.style.animationDelay = `${index * delay}ms`;
+      item.classList.add("appear");
     }, index * delay);
   });
 
@@ -31,13 +17,3 @@
     video.currentTime = 0;
     video.play();
   });
-
-  // const video2 = document.getElementById("second");
-
-  // video2.src =
-  //   "./source/Gen_3_Alpha_Turbo_669305172,_Make_an_action_anima,_DALL·E_2024_10.mp4";
-  // video2.addEventListener("ended", function () {
-  //   video2.currentTime = 0;
-  //   video2.play();
-  // });
-// });
