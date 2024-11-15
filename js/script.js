b017581370fe4e0804283ecba6639503197ef90b
Kakajan <kakajantayjikov@gmail.com>
2024-11-16 04:28:01 +0500

    done

--- js/script.js
+++ js/script.js
@@ -10,47 +10,55 @@ window.addEventListener("load", function () {
 
   const preloader = document.getElementById("preloader");
 
-    // function updateVideoSource() {
-      const videoElement = document.getElementById("main-video");
-      if (window.innerWidth <= 1024) {
-        // создаем массив с URL видео
-        const videos = [
-          "./source/mobile1.mp4",
-          "./source/mobile2.mp4",
-          "./source/mobile3.mp4",
-        ];
+  function updateVideoSource() {
+    const video = document.getElementById("main-video");
+    if (window.innerWidth <= 990) {
+      const videoSources = [
+        "./source/mobile1.mp4",
+        "./source/mobile2.mp4",
+        "./source/mobile3.mp4",
+      ];
 
-        // Создаем элементы <video> для каждого видео и добавляем их на страницу
-        videos.forEach((video) => {
-          videoElement.src = video;
-          videoElement.autoplay = true;
-          videoElement.loop = true;
-          videoElement.muted = true;
-        });
-      } else {
-        video.src = "./source/desktop.mp4";
-        video.style.top = "50%";
-        video.style.left = "50%";
-        video.style.transform = "translate(-50%, -50%)";
-        video.addEventListener("ended", function () {
-          video.currentTime = 0;
-        });
-      }
-      // video.load();
-      video.addEventListener("canplay", function () {
-        preloader.style.display = "none";
-        const texts = document.querySelectorAll(".main-text");
-        let delay = 500;
-        texts.forEach((item, index) => {
-          setTimeout(() => {
-            item.style.opacity = 1;
-            item.style.animationDelay = `${index * delay}ms`;
-            item.classList.add("appear");
-          }, index * delay);
-        });
+      let currentVideoIndex = 0;
+
+      video.addEventListener("ended", () => {
+        currentVideoIndex++;
+        if (currentVideoIndex >= videoSources.length) {
+          currentVideoIndex = 0;
+        }
+        video.src = videoSources[currentVideoIndex];
+        video.play();
       });
-    // }
-    // updateVideoSource();
-    // window.addEventListener("resize", updateVideoSource);
-    // document.body.style.overflow = "auto";
+
+      video.src = videoSources[currentVideoIndex];
+      video.style.top = "unset"
+      video.style.transform = "translate(-40%, 5%)";
+      video.style.scale = "1.2";
+      video.play();
+    } else {
+      video.src = "./source/desktop.mp4";
+      video.style.top = "50%";
+      video.style.left = "50%";
+      video.style.transform = "translate(-50%, -50%)";
+      video.addEventListener("ended", function () {
+        video.currentTime = 0;
+      });
+    }
+    // video.load();
+    video.addEventListener("canplay", function () {
+      preloader.style.display = "none";
+      const texts = document.querySelectorAll(".main-text");
+      let delay = 500;
+      texts.forEach((item, index) => {
+        setTimeout(() => {
+          item.style.opacity = 1;
+          item.style.animationDelay = `${index * delay}ms`;
+          item.classList.add("appear");
+        }, index * delay);
+      });
+    });
+  }
+  updateVideoSource();
+  window.addEventListener("resize", updateVideoSource);
+  document.body.style.overflow = "auto";
 });
